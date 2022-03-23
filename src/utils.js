1224a3408841a7428cf3c3cc103ab26127f154e7
Anurag Singh <sa.anuragsingh@gmail.com>
2022-03-23 11:26:04 +0530

    add utils

---
+++ src/utils.js
@@ -0,0 +1,12 @@
+export const difficulty = {
+  Easy: "a",
+  Medium: "b",
+  Hard: "c",
+};
+
+export const questionTypes = {
+  MCQ: "a",
+  "One Word/Sentence": "b",
+  "Fill in the blanks": "c",
+  "Match the column": "d",
+};
