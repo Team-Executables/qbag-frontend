51fae429514e20068d5a078f8d194e3cb2480fc4
Viraj Patidar <vrj381@gmail.com>
2022-08-12 09:30:16 +0530

    multilingualSupport JSON

--- src/utils.js
+++ src/utils.js
@@ -10,3 +10,70 @@ export const questionTypes = {
   "Fill in the blanks": "c",
   "Match the following": "d",
 };
+
+
+export const multilingualSupport = {
+    "English": {
+        "register": "Register",
+        "login": "Login",
+        "name": "Name",
+        "employment": "Employment",
+        "dragNDropProof": "Drag 'n' Drop or select a valid identity proof",
+        "uploadPhotoID": "Uload Photo ID",
+        "email": "Email Address", 
+        "userType": "User Type",
+        "teacherType": "Teacher",
+        "othersType": "Others",   
+        "schoolCollege": "School/College",       
+        "position": "Position",       
+        "password": "Password",
+        "forgotPassword": "Forgot password?",
+        "dontHaveAnAccountSignUp": "Don't have an account? Sign Up",
+        "alreadyHaveAnAccountSignIn": "Already have an account? Sign in",
+
+        "addQuestion": "Add Question",
+        "searchQuestion": "Search Question",
+        "generateQuestionPaper": "Generate Question Paper",
+
+        "questionDetails": "Question Details",
+        "board": "Board",
+        "grade": "Grade",
+        "questionType": "QuestionType",
+        "mcq": "Multiple Choice Question",
+        "oneWord": "One Word/Sentence",
+        "fillInTheBlanks": "Fill in the Blanks",
+        "matchTheFollowing": "Match the Following",
+        "difficulty": "Difficulty",
+        "easy": "Easy",
+        "medium": "Medium",
+        "hard": "Hard",
+        "marks": "Marks",
+        "subject": "Subject",
+        "typeKeywordsAndPressEnter": "Type Keywords and Press Enter",
+        "question": "Question",
+        "key": "Key",
+        "value": "Value",
+        "addNew": "Add New",
+        "remove": "Remove",
+        "answer": "Answer",
+        "correctOption": "Correct Option",
+        "wrongOption": "Type Wrong Options and Press Enter",
+        "back": "Back",
+        "next": "Next",
+
+        "enterQuestion": "Enter Question",
+        "searchQuestion": "Search Question",
+        "search": "Search",
+        "reviewQuestion": "Review Question",
+        "submitQuestion": "Submit Question",
+
+        "invalidRegistration": "Invalid Registration Data! Please Try Again",
+        "verifyEmail": "An email verification link has been sent on the given email address.\nPlease verify your email to complete the account setup.\nVerification link is valid for 24 hrs",
+
+        "loginError1": "Invalid Login Credentials! Please Try Again",
+        "loginError2": "Please validate your email! Check your inbox"
+    },
+    "Hindi": {
+
+    }
+}
