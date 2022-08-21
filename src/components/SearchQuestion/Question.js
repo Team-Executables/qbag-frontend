2e0f364ddd0a3e365d07db07852aded04b41555b
Viraj Patidar <vrj381@gmail.com>
2022-08-21 11:15:45 +0530

    Question component UI fixed

--- src/components/SearchQuestion/Question.js
+++ src/components/SearchQuestion/Question.js
@@ -7,189 +7,193 @@ import VerifiedIcon from "@mui/icons-material/Verified";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import {
-  Box,
-  FormControl,
-  FormControlLabel,
-  Radio,
-  Paper,
-  RadioGroup,
-  IconButton,
+    Box,
+    FormControl,
+    FormControlLabel,
+    Radio,
+    Paper,
+    RadioGroup,
+    IconButton,
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 import Checkbox from "@mui/material/Checkbox";
 
-const Question = ({ q, qkey, showCheckbox }) => {
-  const multi = useRecoilValue(multilingual);
-  const [upvotes, setUpVotes] = useState(q.upvote);
-  const [downvotes, setDownVotes] = useState(q.downvote);
-  const [selectedQuestions, setSelectedQuestions] =
-    useRecoilState(selectedQues);
+const Question = ({ q, qkey, showCheckbox, showvote }) => {
+    const multi = useRecoilValue(multilingual);
+    const [upvotes, setUpVotes] = useState(q.upvote);
+    const [downvotes, setDownVotes] = useState(q.downvote);
+    const [selectedQuestions, setSelectedQuestions] =
+        useRecoilState(selectedQues);
 
-  const difficulty = {
-    [multi.easy]: "a",
-    [multi.medium]: "b",
-    [multi.hard]: "c",
-  };
+    const difficulty = {
+        [multi.easy]: "a",
+        [multi.medium]: "b",
+        [multi.hard]: "c",
+    };
 
-  function getKeyByValue(object, value) {
-    return Object.keys(object).find((key) => object[key] === value);
-  }
-
-  const isSelected = selectedQuestions.indexOf(q.id);
-
-  function handleVote(str, id) {
-    if (str === "up") {
-      axiosInstance
-        .post(`questions/vote`, {
-          question: id,
-          vote: 1,
-        })
-        .then((res) => {
-          setUpVotes((d) => d + 1);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => window.location.reload());
-    } else {
-      axiosInstance
-        .post(`questions/vote`, {
-          question: id,
-          vote: 0,
-        })
-        .then((res) => {
-          setDownVotes((d) => d + 1);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => window.location.reload());
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find((key) => object[key] === value);
     }
-  }
 
-  const handleCheckboxChange = (e) => {
-    // console.log(e.target.checked);
-    if (e.target.checked) {
-      setSelectedQuestions((selectedQuestions) => {
-        // console.log(selectedQuestions);
-        const qs = JSON.parse(JSON.stringify(selectedQuestions));
-        qs.push(q.id);
-        // console.log(qs);
-        return qs;
-      });
-    } else {
-      setSelectedQuestions((selectedQuestions) => {
-        // console.log(selectedQuestions);
-        let qs = JSON.parse(JSON.stringify(selectedQuestions));
-        const index = qs.indexOf(q.id);
-        if (index > -1) {
-          qs.splice(index, 1);
+    const isSelected = selectedQuestions.indexOf(q.id);
+
+    function handleVote(str, id) {
+        if (str === "up") {
+            axiosInstance
+                .post(`questions/vote`, {
+                    question: id,
+                    vote: 1,
+                })
+                .then((res) => {
+                    setUpVotes((d) => d + 1);
+                    console.log(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => window.location.reload());
+        } else {
+            axiosInstance
+                .post(`questions/vote`, {
+                    question: id,
+                    vote: 0,
+                })
+                .then((res) => {
+                    setDownVotes((d) => d + 1);
+                    console.log(res.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+                .finally(() => window.location.reload());
         }
-        // console.log(qs);
-        return qs;
-      });
     }
-  };
 
-  return (
-    <Paper
-      elevation={3}
-      style={{ backgroundColor: "#e8f5e9", display: "flex", marginTop: 10 }}
-    >
-      {showCheckbox && (
-        <Box my={1.5}>
-          <Checkbox checked={isSelected > -1} onChange={handleCheckboxChange} />
-        </Box>
-      )}
-      <Box m={1}>
-        <Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Box>
-              <Typography variant="h5" sx={{ m: 0, p: 0 }}>
-                {multi.question + " " + (qkey + 1)}
-              </Typography>
+    const handleCheckboxChange = (e) => {
+        // console.log(e.target.checked);
+        if (e.target.checked) {
+            setSelectedQuestions((selectedQuestions) => {
+                // console.log(selectedQuestions);
+                const qs = JSON.parse(JSON.stringify(selectedQuestions));
+                qs.push(q.id);
+                // console.log(qs);
+                return qs;
+            });
+        } else {
+            setSelectedQuestions((selectedQuestions) => {
+                // console.log(selectedQuestions);
+                let qs = JSON.parse(JSON.stringify(selectedQuestions));
+                const index = qs.indexOf(q.id);
+                if (index > -1) {
+                    qs.splice(index, 1);
+                }
+                // console.log(qs);
+                return qs;
+            });
+        }
+    };
+
+    return (
+        <Paper
+            elevation={3}
+            style={{ backgroundColor: "#e8f5e9", display: "flex", marginTop: 25 }}
+        >
+            {showCheckbox && (
+                <Box my={1.5}>
+                    <Checkbox checked={isSelected > -1} onChange={handleCheckboxChange} />
+                </Box>
+            )}
+            <Box m={1} sx={{ width: "100%" }}>
+                <Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between"
+                        }}
+                    >
+                        <Box>
+                            <Typography variant="h5" sx={{ m: 0, p: 0 }}>
+                                {multi.question + " " + (qkey + 1)}
+                            </Typography>
+                        </Box>
+                        {showvote && (
+                            <Box>
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                    }}
+                                    displayPrint="none"
+                                >
+                                    <Box sx={{ mr: 1, mt: 1 }}>
+                                        <IconButton color="primary">
+                                            {q.question_data.setbyTeacher ? <VerifiedIcon /> : ""}
+                                        </IconButton>
+                                    </Box>
+                                    <Box>
+                                        <IconButton
+                                            color="primary"
+                                            aria-label="upvote"
+                                            component="span"
+                                            onClick={() => handleVote("up", q.id)}
+                                        >
+                                            <ThumbUpIcon />
+                                            &nbsp;<span>{upvotes}</span>
+                                        </IconButton>
+                                    </Box>
+                                    <Box>
+                                        <IconButton
+                                            color="primary"
+                                            aria-label="downvote"
+                                            component="span"
+                                            onClick={() => handleVote("down", q.id)}
+                                        >
+                                            <ThumbDownIcon />
+                                            &nbsp;<span>{downvotes}</span>
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            </Box>)
+                        }
+                    </Box>
+                    <Box sx={{ display: "flex" }}>
+                        <Typography variant="h6" sx={{ mr: 2, mt: 0, mb: 1 }}>
+                            {q.question_data.marks}{" "}
+                            {q.question_data.marks > 1 ? (
+                                <>{multi.marks}</>
+                            ) : (
+                                <>{multi.mark}</>
+                            )}{" "}
+                            &nbsp;
+                            {getKeyByValue(difficulty, q.question_data.difficulty)}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Typography variant="h6">{q.question_data.title}</Typography>
+                {q.question_data.type === "d" && <Match data={q.match_data} />}
+                {(q.question_data.type === "b" || q.question_data.type === "c") && (
+                    <Typography variant="h6">
+                        <b>{multi.answer}</b>: {q.option_data[0].option}
+                    </Typography>
+                )}
+                {q.question_data.type === "a" && (
+                    <FormControl component="fieldset">
+                        <RadioGroup value={q.option_data[0].option}>
+                            {q.option_data.map((l) => (
+                                <FormControlLabel
+                                    value={l.option}
+                                    control={<Radio />}
+                                    label={l.option}
+                                    key={l.option}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                )}
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-              }}
-              displayPrint="none"
-            >
-              <Box sx={{ mr: 1, mt: 1 }}>
-                <IconButton color="primary">
-                  {q.question_data.setbyTeacher ? <VerifiedIcon /> : ""}
-                </IconButton>
-              </Box>
-              <Box>
-                <IconButton
-                  color="primary"
-                  aria-label="upvote"
-                  component="span"
-                  onClick={() => handleVote("up", q.id)}
-                >
-                  <ThumbUpIcon />
-                  &nbsp;<span>{upvotes}</span>
-                </IconButton>
-              </Box>
-              <Box>
-                <IconButton
-                  color="primary"
-                  aria-label="downvote"
-                  component="span"
-                  onClick={() => handleVote("down", q.id)}
-                >
-                  <ThumbDownIcon />
-                  &nbsp;<span>{downvotes}</span>
-                </IconButton>
-              </Box>
-            </Box>
-          </Box>
-          <Box sx={{ display: "flex" }}>
-            <Typography variant="h6" sx={{ mr: 2, mt: 0, mb: 1 }}>
-              {q.question_data.marks}{" "}
-              {q.question_data.marks > 1 ? (
-                <>{multi.marks}</>
-              ) : (
-                <>{multi.mark}</>
-              )}{" "}
-              &nbsp;
-              {getKeyByValue(difficulty, q.question_data.difficulty)}
-            </Typography>
-          </Box>
-        </Box>
-        <Typography variant="h6">{q.question_data.title}</Typography>
-        {q.question_data.type === "d" && <Match data={q.match_data} />}
-        {(q.question_data.type === "b" || q.question_data.type === "c") && (
-          <Typography variant="h6">
-            <b>{multi.answer}</b>: {q.option_data[0].option}
-          </Typography>
-        )}
-        {q.question_data.type === "a" && (
-          <FormControl component="fieldset">
-            <RadioGroup value={q.option_data[0].option}>
-              {q.option_data.map((l) => (
-                <FormControlLabel
-                  value={l.option}
-                  control={<Radio />}
-                  label={l.option}
-                  key={l.option}
-                />
-              ))}
-            </RadioGroup>
-          </FormControl>
-        )}
-      </Box>
-    </Paper>
-  );
+        </Paper>
+    );
 };
 
 export default Question;
