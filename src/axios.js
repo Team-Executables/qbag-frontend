1eb40c5685ca97a1ce39e59e995ac86a18641cae
thadaniumang <thadaniumang@outlook.com>
2022-08-25 16:10:11 +0530

    backend connect

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8000/';
+// const baseURL = 'http://127.0.0.1:8000/';
+const baseURL = 'https://qbag-backend.herokuapp.com/';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
