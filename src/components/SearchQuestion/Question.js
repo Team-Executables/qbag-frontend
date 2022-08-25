52bcc703d0d3bef2c0a732859a8fa2437b66f2c9
Viraj Patidar <vrj381@gmail.com>
2022-08-25 23:51:48 +0530

    reason UI

--- src/components/SearchQuestion/Question.js
+++ src/components/SearchQuestion/Question.js
@@ -9,13 +9,13 @@ import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 import ReportIcon from '@mui/icons-material/Report';
 import {
-  Box,
-  FormControl,
-  FormControlLabel,
-  Radio,
-  Paper,
-  RadioGroup,
-  IconButton,
+    Box,
+    FormControl,
+    FormControlLabel,
+    Radio,
+    Paper,
+    RadioGroup,
+    IconButton,
 } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
@@ -29,19 +29,22 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 const Question = ({ q, qkey, showCheckbox, showvote }) => {
-  const multi = useRecoilValue(multilingual);
-  const [upvotes, setUpVotes] = useState(q.upvote);
-  const [downvotes, setDownVotes] = useState(q.downvote);
-  const [selectedQuestions, setSelectedQuestions] =
-    useRecoilState(selectedQues);
+    const multi = useRecoilValue(multilingual);
+    const [upvotes, setUpVotes] = useState(q.upvote);
+    const [reason, setReason] = useState('');
+    const [downvotes, setDownVotes] = useState(q.downvote);
+    const [selectedQuestions, setSelectedQuestions] =
+        useRecoilState(selectedQues);
 
-  const difficulty = {
-    [multi.easy]: "a",
-    [multi.medium]: "b",
-    [multi.hard]: "c",
-  };
+    const difficulty = {
+        [multi.easy]: "a",
+        [multi.medium]: "b",
+        [multi.hard]: "c",
+    };
 
-  const [open, setOpen] = React.useState(false);
+
+
+    const [open, setOpen] = useState(false);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -51,200 +54,239 @@ const Question = ({ q, qkey, showCheckbox, showvote }) => {
         setOpen(false);
     };
 
-  function getKeyByValue(object, value) {
-    return Object.keys(object).find((key) => object[key] === value);
-  }
 
-  const isSelected = selectedQuestions.indexOf(q.id);
+    const [openDownvoteReason, setOpenDownvoteReason] = useState(false);
+    const [ques_id, setQues_id] = useState(0);
 
-  function handleVote(str, id) {
-    if (str === "up") {
-      axiosInstance
-        .post(`questions/vote`, {
-          question: id,
-          vote: 1,
-        })
-        .then((res) => {
-          setUpVotes((d) => d + 1);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => document.location.reload(true));
-    } else {
-      axiosInstance
-        .post(`questions/vote`, {
-          question: id,
-          vote: 0,
-        })
-        .then((res) => {
-          setDownVotes((d) => d + 1);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => document.location.reload(true));
+    const handleClickOpenDownvoteReason = () => {
+        setOpenDownvoteReason(true);
+    };
+
+    const handleCloseDownvoteReason = () => {
+        setOpenDownvoteReason(false);
+    };
+
+
+
+    function getKeyByValue(object, value) {
+        return Object.keys(object).find((key) => object[key] === value);
     }
-  }
 
-  const handleCheckboxChange = (e) => {
-    // console.log(e.target.checked);
-    if (e.target.checked) {
-      setSelectedQuestions((selectedQuestions) => {
-        // console.log(selectedQuestions);
-        const qs = JSON.parse(JSON.stringify(selectedQuestions));
-        qs.push(q.id);
-        // console.log(qs);
-        return qs;
-      });
-    } else {
-      setSelectedQuestions((selectedQuestions) => {
-        // console.log(selectedQuestions);
-        let qs = JSON.parse(JSON.stringify(selectedQuestions));
-        const index = qs.indexOf(q.id);
-        if (index > -1) {
-          qs.splice(index, 1);
+    const isSelected = selectedQuestions.indexOf(q.id);
+
+    function handleUpVote(id) {
+        axiosInstance
+            .post(`questions/vote`, {
+                question: id,
+                vote: 1,
+                reason: '',
+            })
+            .then((res) => {
+                setUpVotes((d) => d + 1);
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => document.location.reload(true));
+    }
+
+    function handleDownVote(id) {
+        axiosInstance
+            .post(`questions/vote`, {
+                question: id,
+                vote: 0,
+                reason: reason,
+            })
+            .then((res) => {
+                setDownVotes((d) => d + 1);
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => document.location.reload(true));
+    }
+
+    const handleCheckboxChange = (e) => {
+        // console.log(e.target.checked);
+        if (e.target.checked) {
+            setSelectedQuestions((selectedQuestions) => {
+                // console.log(selectedQuestions);
+                const qs = JSON.parse(JSON.stringify(selectedQuestions));
+                qs.push(q.id);
+                // console.log(qs);
+                return qs;
+            });
+        } else {
+            setSelectedQuestions((selectedQuestions) => {
+                // console.log(selectedQuestions);
+                let qs = JSON.parse(JSON.stringify(selectedQuestions));
+                const index = qs.indexOf(q.id);
+                if (index > -1) {
+                    qs.splice(index, 1);
+                }
+                // console.log(qs);
+                return qs;
+            });
         }
-        // console.log(qs);
-        return qs;
-      });
-    }
-  };
+    };
 
-  return (
-    <Paper
-      elevation={3}
-      style={{ backgroundColor: "#e8f5e9", display: "flex", marginTop: 25 }}
-    >
-      {showCheckbox && (
-        <Box my={1.5}>
-          <Checkbox checked={isSelected > -1} onChange={handleCheckboxChange} />
-        </Box>
-      )}
-      <Box m={1} sx={{ width: "100%" }}>
-        <Box>
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Box>
-              <Typography variant="h5" sx={{ m: 0, p: 0 }}>
-                {multi.question + " " + (qkey + 1)}
-              </Typography>
-            </Box>
-            {showvote && (
-              <Box>
-                <Box
-                  sx={{
-                    display: "flex",
-                  }}
-                  displayPrint="none"
-                >
-                  <Box sx={{ mr: 1, mt: 1 }}>
-                    <IconButton color="primary">
-                      {q.question_data.setbyTeacher ? <VerifiedIcon /> : ""}
-                    </IconButton>
-                  </Box>
-                  <Box>
-                    <IconButton
-                      color="primary"
-                      aria-label="upvote"
-                      component="span"
-                      onClick={() => handleVote("up", q.id)}
-                    >
-                      <ThumbUpIcon />
-                      &nbsp;<span>{upvotes}</span>
-                    </IconButton>
-                  </Box>
-                  <Box>
-                    <IconButton
-                      color="primary"
-                      aria-label="downvote"
-                      component="span"
-                      onClick={() => handleVote("down", q.id)}
-                    >
-                      <ThumbDownIcon />
-                      &nbsp;<span>{downvotes}</span>
-                    </IconButton>
-                  </Box>
-                  <Box
-                    component="span"
-                    m={1}
-                    display="flex"
-                    justifyContent="space-between"
-                    alignItems="center"
-                  >
-                  <IconButton variant="outlined" onClick={handleClickOpen}>
-                    <ReportIcon />
-                  </IconButton>
-                  </Box>
+    return (
+        <Paper
+            elevation={3}
+            style={{ backgroundColor: "#e8f5e9", display: "flex", marginTop: 25 }}
+        >
+            {showCheckbox && (
+                <Box my={1.5}>
+                    <Checkbox checked={isSelected > -1} onChange={handleCheckboxChange} />
                 </Box>
-              </Box>
             )}
-          </Box>
-          <Box sx={{ display: "flex" }}>
-            <Typography variant="h6" sx={{ mr: 2, mt: 0, mb: 1 }}>
-              {q.question_data.marks}{" "}
-              {q.question_data.marks > 1 ? (
-                <>{multi.marks}</>
-              ) : (
-                <>{multi.mark}</>
-              )}{" "}
-              &nbsp;
-              {getKeyByValue(difficulty, q.question_data.difficulty)}
-            </Typography>
-          </Box>
-        </Box>
-        <Typography variant="h6">{q.question_data.title}</Typography>
-        {q.question_data.type === "d" && <Match data={q.match_data} />}
-        {(q.question_data.type === "b" || q.question_data.type === "c") && (
-          <Typography variant="h6">
-            <b>{multi.answer}</b>: {q.option_data[0].option}
-          </Typography>
-        )}
-        {q.question_data.type === "a" && (
-          <FormControl component="fieldset">
-            <RadioGroup value={q.option_data[0].option}>
-              {q.option_data.map((l) => (
-                <FormControlLabel
-                  value={l.option}
-                  control={<Radio />}
-                  label={l.option}
-                  key={l.option}
-                />
-              ))}
-            </RadioGroup>
-          </FormControl>
-        )}
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle>Report Question</DialogTitle>
-          <DialogContent>
-          <DialogContentText>
-            Please provide a reason for reporting the question below.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="reason"
-            label="Reason for reporting"
-            type="text"
-            fullWidth
-            variant="standard"
-            />
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Cancel</Button>
-              <Button onClick={handleClose}>Report</Button>
-            </DialogActions>
-          </Dialog>
-      </Box>
-    </Paper>
-  );
+            <Box m={1} sx={{ width: "100%" }}>
+                <Box>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Box>
+                            <Typography variant="h5" sx={{ m: 0, p: 0 }}>
+                                {multi.question + " " + (qkey + 1)}
+                            </Typography>
+                        </Box>
+                        {showvote && (
+                            <Box>
+                                <Box
+                                    sx={{
+                                        display: "flex",
+                                    }}
+                                    displayPrint="none"
+                                >
+                                    <Box sx={{ mr: 1, mt: 1 }}>
+                                        <IconButton color="primary">
+                                            {q.question_data.setbyTeacher ? <VerifiedIcon /> : ""}
+                                        </IconButton>
+                                    </Box>
+                                    <Box>
+                                        <IconButton
+                                            color="primary"
+                                            aria-label="upvote"
+                                            component="span"
+                                            onClick={() => handleUpVote(q.id)}
+                                        >
+                                            <ThumbUpIcon />
+                                            &nbsp;<span>{upvotes}</span>
+                                        </IconButton>
+                                    </Box>
+                                    <Box>
+                                        <IconButton
+                                            color="primary"
+                                            aria-label="downvote"
+                                            component="span"
+                                            onClick={() => {setQues_id(q.id); handleClickOpenDownvoteReason()}}
+                                        >
+                                            <ThumbDownIcon />
+                                            &nbsp;<span>{downvotes}</span>
+                                        </IconButton>
+                                    </Box>
+                                    <Box
+                                        component="span"
+                                        m={1}
+                                        display="flex"
+                                        justifyContent="space-between"
+                                        alignItems="center"
+                                    >
+                                        <IconButton variant="outlined" onClick={handleClickOpen}>
+                                            <ReportIcon />
+                                        </IconButton>
+                                    </Box>
+                                </Box>
+                            </Box>
+                        )}
+                    </Box>
+                    <Box sx={{ display: "flex" }}>
+                        <Typography variant="h6" sx={{ mr: 2, mt: 0, mb: 1 }}>
+                            {q.question_data.marks}{" "}
+                            {q.question_data.marks > 1 ? (
+                                <>{multi.marks}</>
+                            ) : (
+                                <>{multi.mark}</>
+                            )}{" "}
+                            &nbsp;
+                            {getKeyByValue(difficulty, q.question_data.difficulty)}
+                        </Typography>
+                    </Box>
+                </Box>
+                <Typography variant="h6">{q.question_data.title}</Typography>
+                {q.question_data.type === "d" && <Match data={q.match_data} />}
+                {(q.question_data.type === "b" || q.question_data.type === "c") && (
+                    <Typography variant="h6">
+                        <b>{multi.answer}</b>: {q.option_data[0].option}
+                    </Typography>
+                )}
+                {q.question_data.type === "a" && (
+                    <FormControl component="fieldset">
+                        <RadioGroup value={q.option_data[0].option}>
+                            {q.option_data.map((l) => (
+                                <FormControlLabel
+                                    value={l.option}
+                                    control={<Radio />}
+                                    label={l.option}
+                                    key={l.option}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                )}
+                {/* <Dialog open={open} onClose={handleClose}>
+                    <DialogTitle>Report Question</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please provide a reason for reporting the question below.
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="reasonReport"
+                            label="Reason for reporting"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleClose}>Cancel</Button>
+                        <Button onClick={handleClose}>Report</Button>
+                    </DialogActions>
+                </Dialog> */}
+
+
+                <Dialog open={openDownvoteReason} onClose={handleCloseDownvoteReason}>
+                    <DialogTitle>Reason for Downvote</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            Please provide a reason for downvoting the question
+                        </DialogContentText>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="reasonDownvote"
+                            label="Reason for downvoting"
+                            type="text"
+                            fullWidth
+                            variant="standard"
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseDownvoteReason}>Cancel</Button>
+                        <Button onClick={() => { handleDownVote(ques_id) }}>Submit</Button>
+                    </DialogActions>
+                </Dialog>
+            </Box>
+        </Paper>
+    );
 };
 
 export default Question;
