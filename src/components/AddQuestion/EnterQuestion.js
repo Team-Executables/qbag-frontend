fb1f4dda21e50b42276d5d21412dbb2427f5e307
Anurag Singh <sa.anuragsingh@gmail.com>
2022-08-25 18:13:55 +0530

    speech to text integrated

--- src/components/AddQuestion/EnterQuestion.js
+++ src/components/AddQuestion/EnterQuestion.js
@@ -10,226 +10,251 @@ import TextField from "@mui/material/TextField";
 // import InputLabel from "@mui/material/InputLabel";
 // import Select from "@mui/material/Select";
 // import MenuItem from "@mui/material/MenuItem";
-import { ButtonGroup } from '@mui/material';
+import { ButtonGroup } from "@mui/material";
 import { Button } from "@mui/material";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { question, MCQoptions, matchPairs, multilingual } from "../../atoms";
 
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const EnterQuestion = ({ qType }) => {
-    const [ques, setQues] = useRecoilState(question);
-    const [mcqs, setMcqs] = useRecoilState(MCQoptions);
-    const tagz = mcqs.map((m) => m.option);
-    const [tags, setTags] = useState([...tagz.splice(1)]);
-    const [matchFields, setMatchFields] = useRecoilState(matchPairs);
-    const multi = useRecoilValue(multilingual);
+  const [ques, setQues] = useRecoilState(question);
+  const [mcqs, setMcqs] = useRecoilState(MCQoptions);
+  const tagz = mcqs.map((m) => m.option);
+  const [tags, setTags] = useState([...tagz.splice(1)]);
+  const [matchFields, setMatchFields] = useRecoilState(matchPairs);
+  const multi = useRecoilValue(multilingual);
 
-    const {
-        transcript,
-        listening,
-        resetTranscript,
-        browserSupportsSpeechRecognition
-      } = useSpeechRecognition();
-    
-      if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
-      }
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
 
-    const addMatchField = () => {
-        setMatchFields([
-            ...matchFields,
-            {
-                key: '',
-                value: ''
-            }
-        ])
-    }
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
 
-    const removeMatchFields = (index) => {
-        const rows = [...matchFields];
-        rows.splice(index, 1);
-        setMatchFields(rows);
-    }
+  const addMatchField = () => {
+    setMatchFields([
+      ...matchFields,
+      {
+        key: "",
+        value: "",
+      },
+    ]);
+  };
 
-    const handleMatchChange = (index, evnt) => {
-        const { name, value } = evnt.target;
-        let list = [...JSON.parse(JSON.stringify(matchFields))];
-        list[index][name] = value;
-        console.log(list);
-        setMatchFields(list);
-    }
+  const removeMatchFields = (index) => {
+    const rows = [...matchFields];
+    rows.splice(index, 1);
+    setMatchFields(rows);
+  };
 
-    const handleQuesChange = (e) => {
-        console.log(ques.title);
-        setQues((ques) => ({
-            ...ques,
-            title: e.target.value,
-        }));
-    };
+  const handleMatchChange = (index, evnt) => {
+    const { name, value } = evnt.target;
+    let list = [...JSON.parse(JSON.stringify(matchFields))];
+    list[index][name] = value;
+    console.log(list);
+    setMatchFields(list);
+  };
 
-    const handleTextAnsChange = (e) => {
-        setQues((q) => ({
-            ...q,
-            options: [
-                {
-                    option: e.target.value,
-                    correct: true,
-                },
-            ],
-        }));
-        console.log(ques.options);
-    };
+  const handleQuesChange = (e) => {
+    console.log(ques.title);
+    setQues((ques) => ({
+      ...ques,
+      title: e.target.value,
+    }));
+  };
 
-    const handleCorrectMCQChange = (e) => {
-        setMcqs((m) => {
-            let newM = [...JSON.parse(JSON.stringify(m))];
-            newM[0].option = e.target.value;
-            console.log(newM);
-            return newM;
-        });
-    };
+  const handleTextAnsChange = (e) => {
+    setQues((q) => ({
+      ...q,
+      options: [
+        {
+          option: e.target.value,
+          correct: true,
+        },
+      ],
+    }));
+    console.log(ques.options);
+  };
 
-    const handleWrongMCQChange = (newTags) => {
-        console.log(newTags);
-        const newWrongMcq = newTags.map((m) => ({
-            option: m,
-            correct: false,
-        }));
-        console.log(newWrongMcq);
-        setMcqs((m) => {
-            let newM = [];
-            newM[0] = JSON.parse(JSON.stringify(m[0]));
-            newM = newM.concat(newWrongMcq);
-            console.log(newM);
-            return newM;
-        });
-        setTags(newTags);
-    };
+  const handleCorrectMCQChange = (e) => {
+    setMcqs((m) => {
+      let newM = [...JSON.parse(JSON.stringify(m))];
+      newM[0].option = e.target.value;
+      console.log(newM);
+      return newM;
+    });
+  };
 
-    return (
-        <>
-            <Typography variant="h6" gutterBottom>
-                {multi.enterQuestion}
-            </Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                    <TextField
-                        required
-                        id="title"
-                        name="title"
-                        label={multi.question}
-                        fullWidth
-                        variant="standard"
-                        value={ques.title}
-                        onChange={handleQuesChange}
-                    />
-                    <div>
-                        <p>Microphone: {listening ? 'on' : 'off'}</p>
-                        <ButtonGroup variant="outlined" aria-label="outlined button group">
-                            <Button onClick={SpeechRecognition.startListening}>Start</Button>
-                            <Button onClick={SpeechRecognition.stopListening}>Stop</Button>
-                            <Button onClick={resetTranscript}>Reset</Button>
-                        </ButtonGroup>
-                        <p>{transcript}</p>
-                    </div>
-                </Grid>
-                <Grid item xs={12}>
-                    {qType === "a" ? (
-                        <>
-                            <TextField
-                                required
-                                id="corr-option"
-                                name="corr-option"
-                                label={multi.correctOption}
-                                value={mcqs[0].option}
-                                fullWidth
-                                variant="standard"
-                                onChange={handleCorrectMCQChange}
-                            />
-                            <Box sx={{ mt: 4 }}>
-                                <ReactTagInput
-                                    tags={tags}
-                                    onChange={(newTags) => handleWrongMCQChange(newTags)}
-                                    placeholder={multi.wrongOption}
-                                />
-                            </Box>
-                        </>
-                    ) : qType === "c" || qType === "b" ? (
+  const handleWrongMCQChange = (newTags) => {
+    console.log(newTags);
+    const newWrongMcq = newTags.map((m) => ({
+      option: m,
+      correct: false,
+    }));
+    console.log(newWrongMcq);
+    setMcqs((m) => {
+      let newM = [];
+      newM[0] = JSON.parse(JSON.stringify(m[0]));
+      newM = newM.concat(newWrongMcq);
+      console.log(newM);
+      return newM;
+    });
+    setTags(newTags);
+  };
+
+  const stopListening = () => {
+    SpeechRecognition.stopListening();
+    setQues((ques) => ({
+      ...ques,
+      title: transcript,
+    }));
+  };
+
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        {multi.enterQuestion}
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <TextField
+            required
+            id="title"
+            name="title"
+            label={multi.question}
+            fullWidth
+            variant="standard"
+            value={ques.title}
+            onChange={handleQuesChange}
+          />
+          <div>
+            <p>Microphone: {listening ? "ON" : "OFF"}</p>
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <Button onClick={SpeechRecognition.startListening}>Start</Button>
+              <Button onClick={stopListening}>Stop</Button>
+              <Button onClick={resetTranscript}>Reset</Button>
+            </ButtonGroup>
+            <p>{transcript}</p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          {qType === "a" ? (
+            <>
+              <TextField
+                required
+                id="corr-option"
+                name="corr-option"
+                label={multi.correctOption}
+                value={mcqs[0].option}
+                fullWidth
+                variant="standard"
+                onChange={handleCorrectMCQChange}
+              />
+              <Box sx={{ mt: 4 }}>
+                <ReactTagInput
+                  tags={tags}
+                  onChange={(newTags) => handleWrongMCQChange(newTags)}
+                  placeholder={multi.wrongOption}
+                />
+              </Box>
+            </>
+          ) : qType === "c" || qType === "b" ? (
+            <TextField
+              required
+              id="Answer"
+              name="Answer"
+              label={multi.answer}
+              fullWidth
+              variant="standard"
+              value={ques.options[0].option}
+              onChange={handleTextAnsChange}
+            />
+          ) : (
+            <div className="container">
+              {matchFields.map((data, index) => {
+                const { key, value } = data;
+                return (
+                  <Box key={index} sx={{ mt: 2 }}>
+                    <div className="col">
+                      <div className="form-group">
+                        {/* <input type="text"  /> */}
                         <TextField
-                            required
-                            id="Answer"
-                            name="Answer"
-                            label={multi.answer}
-                            fullWidth
-                            variant="standard"
-                            value={ques.options[0].option}
-                            onChange={handleTextAnsChange}
+                          label={multi.key}
+                          id="outlined-size-small"
+                          defaultValue="Small"
+                          size="small"
+                          onChange={(evnt) => handleMatchChange(index, evnt)}
+                          value={key}
+                          name="key"
+                          className="form-control"
+                          placeholder={multi.key}
+                          sx={{ mt: 1 }}
                         />
-                    ) : (
-                        <div className="container">
-                            {
-                                matchFields.map((data, index) => {
-                                    const { key, value } = data;
-                                    return (
-                                        <Box key={index} sx={{ mt: 2 }}>
-                                            <div className="col">
-                                                <div className="form-group">
-                                                    {/* <input type="text"  /> */}
-                                                    <TextField
-                                                        label={multi.key}
-                                                        id="outlined-size-small"
-                                                        defaultValue="Small"
-                                                        size="small"
-                                                        onChange={(evnt) => handleMatchChange(index, evnt)}
-                                                        value={key} name="key"
-                                                        className="form-control"
-                                                        placeholder={multi.key}
-                                                        sx={{ mt: 1 }}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col">
-                                                <div className="form-group">
-                                                    {/* <input type="text" onChange={(evnt) => handleMatchChange(index, evnt)} value={value} name="value" className="form-control" placeholder="value" /> */}
-                                                    <TextField
-                                                        label={multi.value}
-                                                        id="outlined-size-small"
-                                                        defaultValue="Small"
-                                                        size="small"
-                                                        className="form-control"
-                                                        onChange={(evnt) => handleMatchChange(index, evnt)}
-                                                        value={value}
-                                                        name="value"
-                                                        placeholder={multi.value}
-                                                        sx={{ mt: 1 }}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className="col">
-                                                {
-                                                    (matchFields.length !== 1) ?
-                                                        <Button variant="outlined" size="small" color="error" sx={{ mt: 1 }} onClick={removeMatchFields}>{multi.remove}</Button> :
-                                                        // <button className="btn btn-outline-danger" onClick={removeMatchFields}>Remove</button> :
-                                                        ''
-                                                }
-                                            </div>
-                                        </Box>
-                                    )
-                                })
-                            }
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <Button variant="contained" size="small" color="success" sx={{ mt: 3 }} onClick={addMatchField}>{multi.addNew}</Button>
-                                    {/* <button className="btn btn-outline-success" onClick={addMatchField}>Add New</button> */}
-                                </div>
-                            </div>
-                        </div>
-                    )}
-                </Grid>
-            </Grid>
-        </>
-    );
+                      </div>
+                    </div>
+                    <div className="col">
+                      <div className="form-group">
+                        {/* <input type="text" onChange={(evnt) => handleMatchChange(index, evnt)} value={value} name="value" className="form-control" placeholder="value" /> */}
+                        <TextField
+                          label={multi.value}
+                          id="outlined-size-small"
+                          defaultValue="Small"
+                          size="small"
+                          className="form-control"
+                          onChange={(evnt) => handleMatchChange(index, evnt)}
+                          value={value}
+                          name="value"
+                          placeholder={multi.value}
+                          sx={{ mt: 1 }}
+                        />
+                      </div>
+                    </div>
+                    <div className="col">
+                      {matchFields.length !== 1 ? (
+                        <Button
+                          variant="outlined"
+                          size="small"
+                          color="error"
+                          sx={{ mt: 1 }}
+                          onClick={removeMatchFields}
+                        >
+                          {multi.remove}
+                        </Button>
+                      ) : (
+                        // <button className="btn btn-outline-danger" onClick={removeMatchFields}>Remove</button> :
+                        ""
+                      )}
+                    </div>
+                  </Box>
+                );
+              })}
+              <div className="row">
+                <div className="col-sm-12">
+                  <Button
+                    variant="contained"
+                    size="small"
+                    color="success"
+                    sx={{ mt: 3 }}
+                    onClick={addMatchField}
+                  >
+                    {multi.addNew}
+                  </Button>
+                  {/* <button className="btn btn-outline-success" onClick={addMatchField}>Add New</button> */}
+                </div>
+              </div>
+            </div>
+          )}
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default EnterQuestion;
